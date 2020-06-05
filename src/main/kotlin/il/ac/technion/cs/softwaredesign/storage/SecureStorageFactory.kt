6d5e86e81996f37cb10fd33bc28b52ef5a7b1b86
Shahakbk <shahakbk@gmail.com>
2020-06-05 16:26:22 +0300

    fixed project structure

--- src/main/kotlin/il/ac/technion/cs/softwaredesign/storage/SecureStorageFactory.kt
+++
@@ -1,7 +0,0 @@
-package il.ac.technion.cs.softwaredesign.storage
-
-import java.util.concurrent.CompletableFuture
-
-interface SecureStorageFactory {
-    fun open(name: ByteArray): CompletableFuture<SecureStorage>
-}
